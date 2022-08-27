edd9bd43c81ba774f56e36194bb46102ff614601
0xcodercrane <codercrane@gmail.com>
2022-08-27 10:27:49 +0800

    Fix

--- bin/download_artifact.js
+++ bin/download_artifact.js
@@ -1,18 +1,18 @@
-module.exports = (github, context) => {
-    const fs = require('fs');
-    const artifacts = await github.actions.listWorkflowRunArtifacts({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        run_id: github.event.workflow_run.id,
-    });
-    const matchArtifact = artifacts.data.artifacts.filter((artifact) => {
-        return artifact.name == "pr"
-    })[0];
-    const download = await github.actions.downloadArtifact({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        artifact_id: matchArtifact.id,
-        archive_format: 'zip',
-    });
-    fs.writeFileSync(`${github.workspace}/pr.zip`, Buffer.from(download.data));
-}
+module.exports = async (github, context) => {
+  const fs = require("fs");
+  const artifacts = await github.actions.listWorkflowRunArtifacts({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: github.event.workflow_run.id,
+  });
+  const matchArtifact = artifacts.data.artifacts.filter((artifact) => {
+    return artifact.name == "pr";
+  })[0];
+  const download = await github.actions.downloadArtifact({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    artifact_id: matchArtifact.id,
+    archive_format: "zip",
+  });
+  fs.writeFileSync(`${github.workspace}/pr.zip`, Buffer.from(download.data));
+};
